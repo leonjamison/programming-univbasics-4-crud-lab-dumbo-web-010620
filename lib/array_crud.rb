b438921af2d1a7f0c18955dd66c7f31ace63fd16
Leon Jamison <leonjamison@icloud.com>
2019-12-31 23:17:34 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   array = ["bill", "hill", "jill", "lil"]
-  array.push("nill")
+  array.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
